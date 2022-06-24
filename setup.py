6f9fe027175d6a76b1495a4f2e19b82519f54b12
DeVriesMatt <matthew.devries.13@gmail.com>
2022-06-24 15:16:43 +0100

    Bump version: 0.0.4-rc0 → 0.0.5-rc0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setup(
     name="cellshape-voxel",
-    version="0.0.4-rc0",
+    version="0.0.5-rc0",
     description="3D shape analysis using deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
